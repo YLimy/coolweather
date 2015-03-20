dbcb71f9b582a69708f43cc1e385504f4080cee3
YLimy <caaiimeenngg@163.com>
2015-03-20 17:22:02 +0800

    20150320

--- src/com/coolweather/app/util/Utility.java
+++ src/com/coolweather/app/util/Utility.java
@@ -1,5 +1,8 @@
 package com.coolweather.app.util;
 
+import org.json.JSONObject;
+
+import android.content.Context;
 import android.text.TextUtils;
 
 import com.coolweather.app.db.CoolWeatherDB;
@@ -88,5 +91,25 @@ public class Utility {
 			}
 		}
 		return false;
+	}
+	
+	/**
+	 * 解析服务器返回的JSON数据，并将解析的结果存储到本地
+	 * @param context
+	 * @param response
+	 */
+	public static void handleWeatherResponse(Context context, String response){
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weatherDesp = weatherInfo.getString("weather");
+			String publishTime = weatherInfo.getString("ptime");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 }
